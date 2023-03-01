9a2a3a7c8087f3cdb73b3e1d45f3edba1eed6572
StAandrew <dev.staniukynas@gmail.com>
2023-03-01 15:46:37 +0000

    fixed syntax

--- server.py
+++ server.py
@@ -1,44 +1,38 @@
 #!/usr/bin/env python3
-
 import socket
-
-from _thread import *
+from _thread import start_new_thread
 import threading
 
-print_lock = threading.Lock()
+
+HOST = "127.0.0.1"
+PORT = 65432
 
 
-#Thread function
-def threaded(conn):
-    while True:
+def threaded(conn, lock):
+    continue_loop = True
+    while continue_loop:
         data = conn.recv(1024)
         if not data:
-            print('Bye')
-            #release lock before breaking
-            print_lock.release()
-            break
-
-        #Send data back to client
-        conn.send(data)
+            print("Bye")
+            lock.release()  # release lock before breaking
+            continue_loop = False
+        conn.send(data)  # send data back to client
     conn.close()
 
-def Main():
-    HOST = '127.0.0.1'
-    PORT = 65432
+
+def main():
+    print_lock = threading.Lock()
 
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         s.bind((HOST, PORT))
         print("Socket binded to port")
-
         s.listen()
-
         while True:
             conn, addr = s.accept()
             print_lock.acquire()
-            print("Connected to:", addr[0], ':', addr[1])
+            print("Connected to:", addr[0], ":", addr[1])
+            start_new_thread(threaded, (conn, print_lock,))
 
-            start_new_thread(threaded, (conn,))
-        
 
-if __name__ == '__main__':
-    Main()
+if __name__ == "__main__":
+    main()
