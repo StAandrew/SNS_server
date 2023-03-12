d02dc27369d4cb56e7bfbf8366149282ed171df0
AlWestwood1 <zceeawe@ucl.ac.uk>
2023-03-12 02:05:12 +0000

    added selections

--- server.py
+++ server.py
@@ -5,20 +5,42 @@ import threading
 
 
 HOST = "127.0.0.1"
-PORT = 65432
+PORT = 65433
 
+def fetch_stock(stock_name):
+    #FETCH STOCK CODE HERE
+    stock_data = "Data about %s." % (stock_name)
+    return stock_data
+
+def predict_stock(stock_name):
+    #PREDICT STOCK CODE HERE
+    stock_predictions = "Predictions about %s" % (stock_name)
+    return stock_predictions
+
+def daily_rundown():
+    #DAILY RUNDOWN CODE HERE
+    rundown = "Daily rundown here"
+    return rundown
 
 def client_connection_thread(conn, port, lock):
     continue_loop = True
     while continue_loop:
         data = conn.recv(1024)
         if data:
-            print("Received:", str(data.decode("ascii")))
+            decoded = data.decode().split(',')
+            print("Received:", decoded)
+            choice, stock_name = decoded
+            if choice == '1':
+                data_out = fetch_stock(stock_name)
+            elif choice == '2':
+                data_out = predict_stock(stock_name)
+            elif choice == '3':
+                data_out = daily_rundown()
         else:
             print(f"Closing connection on port {port}")
             lock.release()  # release lock before breaking
             continue_loop = False
-        conn.send(data)  # send data back to client
+        conn.send(data_out.encode())  # send data back to client
     conn.close()
 
 
