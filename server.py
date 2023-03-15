fece1d0277cd8164849a68ce7b8680c310796199
AlWestwood1 <zceeawe@ucl.ac.uk>
2023-03-15 23:13:59 +0000

    added option to add new stock

--- server.py
+++ server.py
@@ -7,6 +7,11 @@ import threading
 HOST = "127.0.0.1"
 PORT = 65433
 
+def new_stock(stock_name):
+    #FETCH STOCK CODE HERE
+    stock_data = "New stock %s saved successfully." % (stock_name)
+    return stock_data
+
 def fetch_stock(stock_name):
     #FETCH STOCK CODE HERE
     stock_data = "Data about %s." % (stock_name)
@@ -16,6 +21,7 @@ def predict_stock(stock_name):
     #PREDICT STOCK CODE HERE
     stock_predictions = "Predictions about %s" % (stock_name)
     return stock_predictions
+
 
 def daily_rundown():
     #DAILY RUNDOWN CODE HERE
@@ -31,10 +37,12 @@ def client_connection_thread(conn, port, lock):
             print("Received:", decoded)
             choice, stock_name = decoded
             if choice == '1':
-                data_out = fetch_stock(stock_name)
+                data_out = new_stock(stock_name)
             elif choice == '2':
-                data_out = predict_stock(stock_name)
+                data_out = fetch_stock(stock_name)
             elif choice == '3':
+                data_out = predict_stock(stock_name)
+            elif choice == '4':
                 data_out = daily_rundown()
         else:
             print(f"Closing connection on port {port}")
