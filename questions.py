db944a59fd64dee22e7b38e6e0c7c8211e691045
Rafa Asensio <rafa@rafas-air-2.home>
2023-03-28 14:09:41 +0200

    added portfolio optimisation

--- questions.py
+++ questions.py
@@ -2,6 +2,7 @@ import numpy as np # linear algebra
 import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
 import matplotlib.pyplot as plt
 import os
+from scipy.optimize import minimize
 from rnn_model import get_prediction
 
 
@@ -44,11 +45,37 @@ def get_sharpe(ticker, days):
     sharpe = avg_return / vol
     return sharpe
 
-# Stocks/bonds portfolio breakdown for optimising 
-def stocks_or_bonds(days, metric):
-    stocks = 'IVV'  # SP500 ETF
-    bonds = 'TLT'   # Treasury ETF
-    stock_return = get_return(stocks, days)
-    bond_return = get_return(bonds, days)
-    stock_pred = get_prediction(stocks, days)
-    bond_pred = get_prediction(bonds, days)
+# Optimises portfolio of tickers for minimum variance
+def min_var_portfolio(ticker_list, days):
+    
+    returns_list = []
+    for ticker in ticker_list:
+        returns_list.append(get_daily_returns(ticker, days))
+
+    combined_returns = np.array(returns_list).T
+    cov_matrix = np.cov(combined_returns.T)
+
+    # Define the objective function
+    def objective_function(weights, cov_matrix):
+        return np.dot(weights.T, np.dot(cov_matrix, weights))
+    
+    # Define the constraints
+    def constraint_function(weights):
+        return np.sum(weights) - 1.0
+    
+    # Define initial weights as equal weights
+    n_assets = len(combined_returns)
+    init_weights = np.ones(n_assets) / n_assets
+    
+    # Define the bounds for the optimization
+    bounds = tuple((0, 1) for i in range(n_assets))
+    
+    # Define the constraints for the optimization
+    constraints = ({'type': 'eq', 'fun': constraint_function})
+    
+    # Use quadratic programming to minimize the variance
+    result = minimize(objective_function, init_weights, args=cov_matrix, bounds=bounds,
+                      constraints=constraints, method='SLSQP')
+    
+    # Return the optimal weights
+    return result.x
