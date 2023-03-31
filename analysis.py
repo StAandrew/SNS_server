54ff252540d7195a92985a647715e238d279b59b
StAandrew <dev.staniukynas@gmail.com>
2023-03-31 14:01:32 +0100

    merge resolved (2)

--- analysis.py
+++ analysis.py
@@ -1,5 +1,5 @@
-import numpy as np # linear algebra
-import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
+import numpy as np  # linear algebra
+import pandas as pd  # data processing, CSV file I/O (e.g. pd.read_csv)
 import matplotlib.pyplot as plt
 import os
 from scipy.optimize import minimize
@@ -8,29 +8,33 @@ from rnn_model import get_prediction
 
 # Price in day D
 def get_price(ticker, day):
-    prediction = get_prediction(ticker, day)['Close'].tolist()
-    return prediction[day-1]
+    prediction = get_prediction(ticker, day)["Close"].tolist()
+    return prediction[day - 1]
+
 
 # Return over the next D days
 def get_return(ticker, days):
-    prediction = get_prediction(ticker, days)['Close'].tolist()
-    profit = (prediction[days-1] - prediction[0]) / prediction[0]
+    prediction = get_prediction(ticker, days)["Close"].tolist()
+    profit = (prediction[days - 1] - prediction[0]) / prediction[0]
     return profit
+
 
 # Daily return over the next D days
 def get_daily_returns(ticker, days):
-    prediction = get_prediction(ticker, days)['Close'].tolist()
+    prediction = get_prediction(ticker, days)["Close"].tolist()
     daily_returns = []
-    for i in range(days-1):
-        daily_return = prediction[i+1] - prediction[i]
+    for i in range(days - 1):
+        daily_return = prediction[i + 1] - prediction[i]
         daily_returns.append(daily_return)
 
     return daily_returns
+
 
 # Average daily return over the next D days
 def get_avg_daily_return(ticker, days):
     daily_returns = get_daily_returns(ticker, days)
     return np.mean(daily_returns)
+
 
 # Volatility over the next D days
 def get_std(ticker, days):
@@ -38,12 +42,14 @@ def get_std(ticker, days):
     vol = np.std(prediction)
     return vol
 
+
 # Sharpe ratio of stock over the next D days
 def get_sharpe(ticker, days, rfr):
     avg_return = get_avg_daily_return(ticker, days)
     vol = get_std(ticker, days)
     sharpe = (avg_return - rfr) / vol
     return sharpe
+
 
 # Get portfolio returns over the next D days
 def get_portfolio_returns(ticker_list, days):
@@ -55,6 +61,7 @@ def get_portfolio_returns(ticker_list, days):
 
     return combined_returns
 
+
 # Optimises portfolio of tickers for minimum variance
 def min_var_portfolio(combined_returns):
     
@@ -63,21 +70,21 @@ def min_var_portfolio(combined_returns):
     # Define the objective function
     def objective_function(weights, cov_matrix):
         return np.dot(weights.T, np.dot(cov_matrix, weights))
-    
+
     # Define the constraints
     def constraint_function(weights):
         return np.sum(weights) - 1.0
-    
+
     # Define initial weights as equal weights
     n_assets = combined_returns.shape[1]
     init_weights = np.ones(n_assets) / n_assets
-    
+
     # Define the bounds for the optimization
     bounds = tuple((0, 1) for i in range(n_assets))
-    
+
     # Define the constraints for the optimization
-    constraints = ({'type': 'eq', 'fun': constraint_function})
-    
+    constraints = {"type": "eq", "fun": constraint_function}
+
     # Use quadratic programming to minimize the variance
     result = minimize(objective_function, init_weights, args=cov_matrix, bounds=bounds, constraints=constraints, method='SLSQP')
     
@@ -86,6 +93,7 @@ def min_var_portfolio(combined_returns):
     min_var = result.fun
 
     return opt_weights, min_var
+
 
 # Sharpe ratio of portfolio over the next D days
 def get_portfolio_sharpe(weights, combined_returns, rfr):
@@ -98,7 +106,8 @@ def get_portfolio_sharpe(weights, combined_returns, rfr):
 
     sharpe = (total_return - rfr) / total_vol
 
-    return -sharpe      # negative return because we use it to minimise
+    return -sharpe  # negative return because we use it to minimise
+
 
 # Optimises portfolio of tickers for maximum sharpe
 def max_sharpe_portfolio(combined_returns, rfr):
@@ -108,11 +117,18 @@ def max_sharpe_portfolio(combined_returns, rfr):
     init_weights = np.ones(num_stocks) / num_stocks
 
     # Define the optimization constraints
-    constraints = ({'type': 'eq', 'fun': lambda x: np.sum(x) - 1})
-    bounds = tuple((0,1) for i in range(num_stocks))
+    constraints = {"type": "eq", "fun": lambda x: np.sum(x) - 1}
+    bounds = tuple((0, 1) for i in range(num_stocks))
 
     # Run the optimization
-    result = minimize(get_portfolio_sharpe, init_weights, args=(combined_returns, rfr), method='SLSQP', bounds=bounds, constraints=constraints)
+    result = minimize(
+        get_portfolio_sharpe,
+        init_weights,
+        args=(combined_returns, rfr),
+        method="SLSQP",
+        bounds=bounds,
+        constraints=constraints,
+    )
 
     # Extract the optimal weights and maximum Sharpe ratio achieved
     opt_weights = result.x
--- server.py
+++ server.py
@@ -1,29 +1,29 @@
 #!/usr/bin/env python3
 
 import socket
-
 from _thread import *
 import threading
 
 print_lock = threading.Lock()
 
 
-#Thread function
+# Thread function
 def threaded(conn):
     while True:
         data = conn.recv(1024)
         if not data:
-            print('Bye')
-            #release lock before breaking
+            print("Bye")
+            # release lock before breaking
             print_lock.release()
             break
 
-        #Send data back to client
+        # Send data back to client
         conn.send(data)
     conn.close()
 
+
 def Main():
-    HOST = '127.0.0.1'
+    HOST = "127.0.0.1"
     PORT = 65432
 
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
@@ -35,10 +35,10 @@ def Main():
         while True:
             conn, addr = s.accept()
             print_lock.acquire()
-            print("Connected to:", addr[0], ':', addr[1])
+            print("Connected to:", addr[0], ":", addr[1])
 
             start_new_thread(threaded, (conn,))
-        
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     Main()
